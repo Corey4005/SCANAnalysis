2ab20b7d07253fab6f012cb0d36b55f57c737035
Corey4005 <cdw0063@uah.edu>
2021-06-23 13:22:14 -0500

    function can now extract 2020 data on local computer. Need to update arguments so that specific years can be selected.

--- ExtractESI.py
+++ ExtractESI.py
@@ -23,30 +23,35 @@ import geopandas as gpd
 import numpy as np
 
 #read in the filepath and metadata
-filepath = 'r' + sys.argv[1]
-metadata = 'r' + sys.argv[2]
+filepath = r'C:\\Users\cwalker\Desktop\Data\ESI_Data\esi_1wk_tif\2020'
+metadata = r'C:\\Users\cwalker\Desktop\Data\Metadata\SCAN_Metadata_AL.csv'
 
 #check to see if len of sys.argv is good. 
-if len(sys.arg) != 3:
-    print('usage: run ExtractESI.py [insert filepath as str], [insert metadata as str]')
-    sys.exit()
-else:
-    pass
+# if len(sys.argv) != 3:
+#     print('usage: run ExtractESI.py [insert filepath as str], [insert metadata as str]')
+#     sys.exit()
+# else:
+#     pass
 
 #look for all files in filepath
-files = glob2.glob(filepath + '/*tif')
+files = glob2.glob(filepath + '/*.tif')
 print(len(files))
 
 #read int the metadata
 import_csv = pd.read_csv(metadata)
 df = pd.DataFrame(import_csv)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
         
 #extract the ESI, datestring, lon and lat for file in filepath
 def ExtractESI(files):
+    ESI = []
+    Date = []
+    Lat = []
+    Lon = []
     for file in files:
         file_split = file.split('_')
-        file_date = file_split[6].split('.tif')[0]
+        file_date = file_split[5].split('.tif')[0]
+        when = dt.datetime.strptime(file_date, '%Y%j').date()
         raster = rasterio.open(file)
         for point in gdf['geometry']:
             x = point.xy[0][0]
@@ -54,12 +59,17 @@ def ExtractESI(files):
             index = raster.index(x, y)
             values = raster.read(1)[index]
             values_list = [values for value in index]
-            ESI_values = values_list[0].tolist()
+            ESI_values = values_list[0]
             formated = float("{:.3f}".format(ESI_values))
-            dates = np.array(file_date)
+            ESI.append(formated)
             longitude = np.array(x)
             latitude = np.array(y)
-            print(formated, dates, longitude, latitude)
+            Date.append(when)
+            Lon.append(longitude)
+            Lat.append(latitude)
+    df = pd.DataFrame(data={'ESI':ESI, 'Latitude':Lat, 'Longitude':Lon, 'Date':Date})
+    df.to_csv('/Users/cwalker/Desktop/Data/Processed_ESI/1_wk_ESI_2020.csv')
+    return
         
 def main():
     ExtractESI(files)
