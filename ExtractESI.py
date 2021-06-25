cb7ceec9b912b0e784ee040c845b9eeedab55ad6
Corey4005 <cdw0063@uah.edu>
2021-06-25 14:07:27 -0500

    changed the code again to make it less redundant

--- ExtractESI.py
+++ ExtractESI.py
@@ -16,32 +16,21 @@ author: Corey
 """
 import rasterio
 import glob2
-import sys 
 import datetime as dt
 import pandas as pd
 import geopandas as gpd
-import numpy as np
 
 #read in the filepath and metadata
 filepath = r'C:\\Users\cwalker\Desktop\Data\ESI_Data\esi_1wk_tif\2020'
 metadata = r'C:\\Users\cwalker\Desktop\Data\Metadata\SCAN_Metadata_AL.csv'
 
-#check to see if len of sys.argv is good. 
-# if len(sys.argv) != 3:
-#     print('usage: run ExtractESI.py [insert filepath as str], [insert metadata as str]')
-#     sys.exit()
-# else:
-#     pass
-
-#####
 #look for all files in filepath
 files = glob2.glob(filepath + '/*.tif')
-print(len(files))
 
 #read int the metadata
 import_csv = pd.read_csv(metadata)
 df = pd.DataFrame(import_csv)
-gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude))
+gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude))
         
 #extract the ESI, datestring, lon and lat for file in filepath
 def ExtractESI(files):
@@ -59,17 +48,14 @@ def ExtractESI(files):
             y = point.xy[1][0]
             index = raster.index(x, y)
             values = raster.read(1)[index]
-            values_list = [values for value in index]
-            ESI_values = values_list[0]
-            formated = float("{:.3f}".format(ESI_values))
+            formated = float("{:.3f}".format(values))
             ESI.append(formated)
-            longitude = np.array(x)
-            latitude = np.array(y)
             Date.append(when)
-            Lon.append(longitude)
-            Lat.append(latitude)
+            Lon.append(x)
+            Lat.append(y)
     df = pd.DataFrame(data={'ESI':ESI, 'Latitude':Lat, 'Longitude':Lon, 'Date':Date})
-    df.to_csv('/Users/cwalker/Desktop/Data/Processed_ESI/1_wk_ESI_2020.csv')
+    print(df)
+    #df.to_csv('/Users/cwalker/Desktop/Data/Processed_ESI/1_wk_ESI_2020.csv')
     return
         
 def main():
