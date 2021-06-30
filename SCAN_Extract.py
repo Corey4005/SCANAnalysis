3127e33f07230dc2bb56357c1dcc3ab114a2ea4c
Corey4005 <cdw0063@uah.edu>
2021-06-30 13:22:55 -0500

    created a basic function that takes takes mean of SCAN Data

--- SCAN_Extract.py
+++ SCAN_Extract.py
@@ -7,20 +7,35 @@ This is a temporary script file.
 
 from pathlib import Path
 import pandas as pd
+import glob2
 
 # create the data path
-SCAN_DATA = Path.home() / 'Desktop' / 'Data' / 'SCAN_Data' / 'SCAN_2057.csv'
-READ_SCAN = pd.read_csv(SCAN_DATA)
-DF = pd.DataFrame(READ_SCAN)
+filepath = r'C:\\Users\cwalker\Desktop\Data\SCAN_Data'
+files = glob2.glob(filepath + '/*.csv')
+
+savepath = Path.home() / 'Desktop' / 'Data' / 'Processed_SCAN'
+
 
 #convert date to dt object
-DF['Date'] = pd.to_datetime(DF['Date'])
 
 
+#create the output filename for each file. 
 #group by 7 days and find mean values
+for file in files: 
+    READ_SCAN = pd.read_csv(file)
+    DF = pd.DataFrame(READ_SCAN)
+    DF['Date'] = pd.to_datetime(DF['Date'])
+    Station = str(DF.loc[1, 'Station Id'])
+    file_name = 'SCAN' + '_' + Station + '.csv'
+    group = DF.groupby(pd.Grouper(key='Date', freq='7D')).mean()
+    new_df = pd.DataFrame(group)
+    filename = savepath / file_name
+    new_df.to_csv(filename)
 
-group = DF.groupby(pd.Grouper(key='Date', freq='7D')).mean()
-new_df = pd.DataFrame(group).to_excel('2057.xlsx')
+    
+    
+
+
 
 
 
