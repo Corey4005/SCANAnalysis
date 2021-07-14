f159544233c4ee74e3744ea03b1ae0c7c0455e73
corey walker <cdw0063@uah.edu>
2021-07-13 23:14:12 -0500

    just updated the script so that it takes into account scientific questions

--- SCAN_avg_ESI_2020.py
+++ SCAN_avg_ESI_2020.py
@@ -1,77 +1,43 @@
 import pandas as pd
-import numpy as np
 
 # Read in the ESI data
 esi = pd.read_csv('/Users/coreywalker/Desktop/NOAA/ESIExtractProject/ESI_1wk_tif2select_pt.csv')
 esi['Date'] = pd.to_datetime(esi['Date'])
-# print(esi.dtype)
-#print(esi.shape) # 19080 rows x 3 columns
-#print(esi.columns)
 
-
-
-# # Read in the SCAN data
+# # put in the scan_path!
 scan_path = '/Users/coreywalker/Desktop/NOAA/SCANAnalysis/SCAN_DEPTHS_ALL.csv'
 scan = pd.read_csv(scan_path)
-#print(scan.columns)
-#print(scan.shape)
-#print(scan.columns)
+
 
 # # Just get the columns we are interested in
 sms = scan[['Date', 'station','SMS-2.0in', 'SMS-4.0in', 'SMS-8.0in', 'SMS-20.0in','SMS-40.0in']]
-sms_test = scan[['Date', 'station', 'SMS-2.0in']]
 
 # # Date column to datetime format
 sms['Date'] = pd.to_datetime(sms['Date'])
-#print(sms.dtypes)
 
-sms_grp = sms.groupby(['station', pd.Grouper(key='Date', freq='W-SUN')]).size()
+#group the dataframe by station and date and agrigate the mean and count for each group.
+sms_grp = sms.groupby(['station', pd.Grouper(key='Date', freq='W-WED')]).agg(['mean', 'count'])
+print(sms_grp.columns)
 
-index = np.where(sms_grp == 7)
-print(index[0])
-#print(sms_test.iloc[index[0]])
-#sms_grp.to_csv('/Users/coreywalker/Desktop/test_group.csv')
-#.mean().reset_index()
-#print(sms_grp.size('Date'))
+#the groupby function creates a multiindex that needs to be joined together.
+sms_grp.columns = sms_grp.columns.map('_'.join)
 
-# print(sms_grp.shape)
-#print(filtered.shape)
-#print(sms_grp['station'].unique())
+#reset the index so that the columns are formatted correctly. 
+sms_grp = sms_grp.reset_index()
+two_in = sms_grp[['station', 'Date', 'SMS-2.0in_mean', 'SMS-2.0in_count']]
 
-# # Get data for year 2020
-# # grouping every week. Make sure you have week prior or after (depending on how get avg.)
-# # MAKE SURE THIS IS CALCULATING THE AVERAGES THE WAY YOU WANT!!! IS THIS AVG. 7DAY PRIOR OR AFTER?
+#now, we filter to get just the weeks that produced a mean from 7 values. 
+sms_2in_index = two_in[two_in['SMS-2.0in_count'] == 7]
 
+#merge the ESI with the sms 2-in index at Date and Station.
+merge_sms_esi = pd.merge(left=esi, right=sms_2in_index, on=['Date', 'station'], how='outer', indicator='how').reset_index()
 
-# sms_2020= sms[(sms['Date'] >= '2020-01-01') & (sms['Date'] <= '2021-01-05')]
-# print(sms_2020.shape) # 5828 X 7
-# print(sms_2020['Date'].nunique()) # 372
-# print(sms_2020['Date'].dt.year.unique())
+#index the dataframe and get only the values where both were merged AND ESI has a real value. 
+both = merge_sms_esi[merge_sms_esi['how']=='both']
+corrected = both[both['ESI'] != -9999]
 
-
-# grp_7 = sms_2020.groupby(['station', pd.Grouper(key='Date', freq='7D')]).mean().reset_index()
-# print(grp_7.shape) # 834 X 7
-# print(grp_7['Date'].nunique()) # 53 unique dates
-# print(grp_7['station'].nunique()) # only 17 stations
-
-# # checking out the dates to see if all looks right.
-# grp_7_dates = grp_7['Date'].dt.date.unique()
-# print(len(grp_7_dates))
-# print(grp_7_dates.max())
-# print(grp_7_dates.min())
-
-# # merge the esi and SCAN
-# sms_esi = pd.merge(left=esi, right=sms_grp, on=['Date', 'station'], how='outer', indicator='how').reset_index()
-
-
-
-# # # This is what we are really interested in:
-# both = sms_esi[sms_esi['how']=='both']
-# print(both['Date'].nunique()) 
-# print(both.shape) 
-# print(both)
-
-# both.to_csv('/Users/coreywalker/Desktop/NOAA/both_merged.csv')
+#send that bad boy to a csv for analysis. 
+corrected.to_csv('/Users/coreywalker/Desktop/NOAA/ALL_SMS_ESI_merged.csv')
 
 
 
