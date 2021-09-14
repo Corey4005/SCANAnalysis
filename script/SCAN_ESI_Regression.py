7679df2e9eb6f8ce2f68040261d008e414c08824
Corey Walker <cwalker@nsstc.uah.edu>
2021-09-14 15:19:47 -0500

    finished the 3 slice analysis and now need to work on summations

--- script/SCAN_ESI_Regression.py
+++ script/SCAN_ESI_Regression.py
@@ -25,12 +25,15 @@ sms = scan[['Date', 'station','SMS-2.0in', 'SMS-4.0in', 'SMS-8.0in', 'SMS-20.0in
 # Date column to datetime format
 sms['Date'] = pd.to_datetime(sms['Date'])
 
-#create rolling averages for 
+#create rolling averages for each level of soil moisture
 sms['2in_rolling_mean'] = sms['SMS-2.0in'].rolling(7, min_periods=3).mean()
 sms['4in_rolling_mean'] = sms['SMS-4.0in'].rolling(7, min_periods=3).mean()
 sms['8in_rolling_mean'] = sms['SMS-8.0in'].rolling(7, min_periods=3).mean()
 sms['20in_rolling_mean'] = sms['SMS-20.0in'].rolling(7, min_periods=3).mean()
 sms['40in_rolling_mean'] = sms['SMS-40.0in'].rolling(7, min_periods=3).mean()
+
+#create rolling sums for each level of soil moisture
+
 
 #create averages for sliced data
 sms['SMS-all-avg'] = (sms['2in_rolling_mean'] + sms['4in_rolling_mean'] + sms['8in_rolling_mean'] + sms['20in_rolling_mean'] + sms['40in_rolling_mean']) / 5
@@ -38,6 +41,11 @@ sms['SMS-2.0+4.0_avg'] = (sms['2in_rolling_mean'] + sms['4in_rolling_mean']) / 2
 sms['SMS-4.0+8.0_avg'] = (sms['4in_rolling_mean'] + sms['8in_rolling_mean']) / 2
 sms['SMS-8.0+20.0_avg'] = (sms['8in_rolling_mean'] + sms['20in_rolling_mean']) / 2
 sms['SMS-20.0+40.0_avg'] = (sms['20in_rolling_mean'] + sms['40in_rolling_mean']) /2
+
+#look at three layers sliced data
+sms['SMS-2.0+4.0+8.0_avg'] = (sms['2in_rolling_mean'] + sms['4in_rolling_mean'] + sms['8in_rolling_mean']) / 3
+sms['SMS-4.0+8.0+20.0_avg'] = (sms['4in_rolling_mean'] + sms['8in_rolling_mean'] + sms['20in_rolling_mean']) / 3
+sms['SMS-8.0+20.0+40.0_avg'] = (sms['8in_rolling_mean'] + sms['20in_rolling_mean'] + sms['40in_rolling_mean']) / 3
 
 #subset sms for each group
 two_in = sms[['station', 'Date', '2in_rolling_mean']]
@@ -50,6 +58,9 @@ two_in_four_in_avg = sms[['station', 'Date', 'SMS-2.0+4.0_avg']]
 four_in_eight_in_avg = sms[['station', 'Date', 'SMS-4.0+8.0_avg']]
 eight_in_twenty_in_avg = sms[['station', 'Date','SMS-8.0+20.0_avg']]
 twenty_in_forty_in_avg = sms[['station', 'Date', 'SMS-20.0+40.0_avg']]
+two_four_eight_in_avg = sms[['station', 'Date', 'SMS-2.0+4.0+8.0_avg']]
+four_eight_twenty_in_avg = sms[['station', 'Date', 'SMS-4.0+8.0+20.0_avg']]
+eight_twenty_forty_in_avg = sms[['station', 'Date', 'SMS-8.0+20.0+40.0_avg']]
 
 #merge ESI and SMS
 two_in_merge = pd.merge(left=esi, right=two_in, on=['Date', 'station'], how='outer')
@@ -62,6 +73,9 @@ two_in_four_in_merge = pd.merge(left=esi, right=two_in_four_in_avg, on=['Date', 
 four_in_eight_in_merge = pd.merge(left=esi, right=four_in_eight_in_avg, on=['Date', 'station'], how='outer')
 eight_in_twenty_in_merge = pd.merge(left=esi, right=eight_in_twenty_in_avg, on=['Date', 'station'], how='outer')
 twenty_in_forty_in_merge = pd.merge(left=esi, right=twenty_in_forty_in_avg, on=['Date', 'station'], how='outer')
+two_four_eight_in_merge = pd.merge(left=esi, right=two_four_eight_in_avg, on=['Date', 'station'], how='outer')
+four_eight_twenty_in_merge = pd.merge(left=esi, right=four_eight_twenty_in_avg, on=['Date', 'station'], how='outer')
+eight_twenty_forty_in_merge = pd.merge(left=esi, right=eight_twenty_forty_in_avg, on=['Date', 'station'], how='outer')
 
 #correct the indexes to get rid of ESI values where there is a bad reading and drop all NaNs. 
 two_corrected = two_in_merge[two_in_merge['ESI'] != -9999].dropna()
@@ -74,6 +88,9 @@ two_in_four_in_corrected = two_in_four_in_merge[two_in_four_in_merge['ESI'] != -
 four_in_eight_in_corrected = four_in_eight_in_merge[four_in_eight_in_merge['ESI'] != -9999].dropna()
 eight_in_twenty_in_corrected = eight_in_twenty_in_merge[eight_in_twenty_in_merge['ESI'] != -9999].dropna()
 twenty_in_forty_in_corrected = twenty_in_forty_in_merge[twenty_in_forty_in_merge['ESI'] != -9999].dropna()
+two_four_eight_in_corrected = two_four_eight_in_merge[two_four_eight_in_merge['ESI'] != -9999].dropna()
+four_eight_twenty_in_corrected = four_eight_twenty_in_merge[four_eight_twenty_in_merge['ESI'] != -9999].dropna()
+eight_twenty_forty_in_corrected = eight_twenty_forty_in_merge[eight_twenty_forty_in_merge['ESI'] != -9999].dropna()
 
 # #create x and y values for eaach group plot.
 two_in_x = two_corrected['ESI']
@@ -215,11 +232,56 @@ ax[1, 4].set_title('20in_40in_slice SMS vs ESI')
 sns.set_context("paper")
 sns.set_style('whitegrid')
 
+#create the three slice plots x and y values
+two_four_eight_in_x = two_four_eight_in_corrected['ESI']
+two_four_eight_in_y = two_four_eight_in_corrected['SMS-2.0+4.0+8.0_avg']
+
+four_eight_twenty_in_x = four_eight_twenty_in_corrected['ESI']
+four_eight_twenty_in_y = four_eight_twenty_in_corrected['SMS-4.0+8.0+20.0_avg']
+
+eight_twenty_forty_in_x = eight_twenty_forty_in_corrected['ESI']
+eight_twenty_forty_in_y = eight_twenty_forty_in_corrected['SMS-8.0+20.0+40.0_avg']
+
+#here is the plot for the overall three slice values. 
+fig2, ax2 = plt.subplots(nrows=1, ncols=3, figsize=(15,10))
+
+#here is the plot for the two_four_eight slice
+sns.regplot(ax=ax2[0], x=two_four_eight_in_x, y=two_four_eight_in_y, data='two_four_eight_in_corrected', scatter_kws={'s':2}, line_kws={'color': 'black'})
+stats_slice_248 = stats.pearsonr(two_four_eight_in_x, two_four_eight_in_y)
+formated_r_slice_248 = ("{:.4f}".format(stats_slice_248[0]))
+formated_p_slice_248 = ("{:.4f}".format(stats_slice_248[1]))
+shape_slice_248 = two_four_eight_in_corrected.shape[0]
+at_slice_248 = AnchoredText(s=f"R2: {formated_r_slice_248} \n P: {formated_p_slice_248} \n n: {shape_slice_248}", loc='upper left')
+ax2[0].add_artist(at_slice_248)
+ax2[0].set_title('two_four_eight_in slice vs ESI')
+
+#here is the plot for the four_eight_twenty slice
+sns.regplot(ax=ax2[1], x=four_eight_twenty_in_x, y=four_eight_twenty_in_y, data='four_eight_twenty_in_corrected', scatter_kws={'s':2}, line_kws={'color': 'black'})
+stats_slice_4820 = stats.pearsonr(four_eight_twenty_in_x, four_eight_twenty_in_y)
+formated_r_slice_4820 = ("{:.4f}".format(stats_slice_4820[0]))
+formated_p_slice_4820 = ("{:.4f}".format(stats_slice_4820[1]))
+shape_slice_4820= four_eight_twenty_in_corrected.shape[0]
+at_slice_4820 = AnchoredText(s=f"R2: {formated_r_slice_4820} \n P: {formated_p_slice_4820} \n n: {shape_slice_4820}", loc='upper left')
+ax2[1].add_artist(at_slice_4820)
+ax2[1].set_title('four_eight_twenty_in slice vs ESI')
+
+#here is the plot for the eight_twenty_forty slice. 
+sns.regplot(ax=ax2[2], x=eight_twenty_forty_in_x, y=eight_twenty_forty_in_y, data='eight_twenty_forty_in_corrected', scatter_kws={'s':2}, line_kws={'color': 'black'})
+stats_slice_82040 = stats.pearsonr(eight_twenty_forty_in_x, eight_twenty_forty_in_y)
+formated_r_slice_82040 = ("{:.4f}".format(stats_slice_82040[0]))
+formated_p_slice_82040 = ("{:.4f}".format(stats_slice_82040[1]))
+shape_slice_82040 = eight_twenty_forty_in_corrected.shape[0]
+at_slice_82040 = AnchoredText(s=f"R2: {formated_r_slice_82040} \n P: {formated_p_slice_82040} \n n: {shape_slice_82040}", loc='upper left')
+ax2[2].add_artist(at_slice_82040)
+ax2[2].set_title('eight_twenty_forty_in slice vs ESI')
+
+
 # #lets try a grid plot to look at individual station stats for each depth. 
 variable_y = 1.0
 
 stations_two = sns.lmplot(x='ESI', y='2in_rolling_mean', data=two_corrected, col='station', height=6, col_wrap=3)
 stations_two.fig.suptitle('ESI (y-axis) by Individual Alabama USDA 2in SMS (x-axis)', y=variable_y)
+
 
 stations_four = sns.lmplot(x='ESI', y='4in_rolling_mean', data=four_corrected, col='station', height=6, col_wrap=3)
 stations_four.fig.suptitle('ESI (y-axis) by Individual Alabama USDA 4in SMS (x-axis)', y=variable_y)
@@ -248,11 +310,23 @@ stations_eight_twenty.fig.suptitle('ESI (y-axis) by Individual Alabama USDA eigh
 stations_twenty_forty = sns.lmplot(x='ESI', y='SMS-20.0+40.0_avg', data=twenty_in_forty_in_corrected, col='station', height=6, col_wrap=3)
 stations_twenty_forty.fig.suptitle('ESI (y-axis) by Individual Alabama USDA twenty_in_forty_in slice avg SMS (x-axis)', y=variable_y)
 
+stations_two_four_eight = sns.lmplot(x='ESI', y='SMS-2.0+4.0+8.0_avg', data=two_four_eight_in_corrected, col='station', height=6, col_wrap=3)
+stations_two.fig.suptitle('ESI (y-axis) by Individual Alabama USDA two_four_eight_in slice SMS (x-axis)', y=variable_y)
+
+stations_four_eight_twenty = sns.lmplot(x='ESI', y='SMS-4.0+8.0+20.0_avg', data=four_eight_twenty_in_corrected, col='station', height=6, col_wrap=3)
+stations_two.fig.suptitle('ESI (y-axis) by Individual Alabama USDA four_eight_twenty_in slice SMS (x-axis)', y=variable_y)
+
+stations_eight_twenty_forty = sns.lmplot(x='ESI', y='SMS-8.0+20.0+40.0_avg', data=eight_twenty_forty_in_corrected, col='station', height=6, col_wrap=3)
+stations_two.fig.suptitle('ESI (y-axis) by Individual Alabama USDA eight_twenty_forty_in slice SMS (x-axis)', y=variable_y)
+
+
 #create the figure functons to annotate the appropriate stats for each graph. 
 def annotate_two(data, **kws):
+    ax = plt.gca()
     r_2, p_2 = stats.pearsonr(data['ESI'], data['2in_rolling_mean'])
     shape2 = data.shape[0]
     ax.text(-2.5, 50, s='r={:.4f}, \n p={:.4g},\n n={shape}'.format(r_2, p_2, shape=shape2))
+
 
 def annotate_four(data, **kws):
     r_4, p_4 = stats.pearsonr(data['ESI'], data['4in_rolling_mean'])
@@ -308,6 +382,24 @@ def annotate_twenty_forty(data, **kws):
     ax = plt.gca()
     ax.text(-2.5, 50, s='r={:.4f}, \n p={:.4g},\n n={shape}'.format(r_twenty_forty, p_twenty_forty, shape=shape_twenty_forty))
 
+def annotate_two_four_eight(data, **kws):
+    r_two_four_eight, p_two_four_eight = stats.pearsonr(data['ESI'], data['SMS-2.0+4.0+8.0_avg'])
+    shape_two_four_eight = data.shape[0]
+    ax = plt.gca()
+    ax.text(-2.5, 50, s='r={:.4f}, \n p={:.4g},\n n={shape}'.format(r_two_four_eight, p_two_four_eight, shape=shape_two_four_eight))
+
+def annotate_four_eight_twenty(data, **kws):
+    r_four_eight_twenty, p_four_eight_twenty = stats.pearsonr(data['ESI'], data['SMS-4.0+8.0+20.0_avg'])
+    shape_four_eight_twenty = data.shape[0]
+    ax = plt.gca()
+    ax.text(-2.5, 50, s='r={:.4f}, \n p={:.4g},\n n={shape}'.format(r_four_eight_twenty, p_four_eight_twenty, shape=shape_four_eight_twenty))
+    
+def annotate_eight_twenty_forty(data, **kws):
+    r_eight_twenty_forty, p_eight_twenty_forty = stats.pearsonr(data['ESI'], data['SMS-8.0+20.0+40.0_avg'])
+    shape_eight_twenty_forty = data.shape[0]
+    ax = plt.gca()
+    ax.text(-2.5, 50, s='r={:.4f}, \n p={:.4g},\n n={shape}'.format(r_eight_twenty_forty, p_eight_twenty_forty, shape=shape_eight_twenty_forty))
+    
 #show all the individual figures so that they can be saved. 
 stations_two.map_dataframe(annotate_two)
 stations_four.map_dataframe(annotate_four)
@@ -319,7 +411,12 @@ stations_two_four.map_dataframe(annotate_two_four)
 stations_four_eight.map_dataframe(annotate_four_eight)
 stations_eight_twenty.map_dataframe(annotate_eight_twenty)
 stations_twenty_forty.map_dataframe(annotate_twenty_forty)
+stations_two_four_eight.map_dataframe(annotate_two_four_eight)
+stations_four_eight_twenty.map_dataframe(annotate_four_eight_twenty)
+stations_eight_twenty_forty.map_dataframe(annotate_eight_twenty_forty)
 
+fig.savefig('All_2slice_SMS+ESI.pdf')
+fig2.savefig('All_3slice_SMS+ESI.pdf')
 stations_two.savefig('stations_two.pdf')
 stations_four.savefig('stations_four.pdf')
 stations_eight.savefig('stations_eight.pdf')
@@ -329,6 +426,9 @@ stations_two_four.savefig('stations_two_four.pdf')
 stations_four_eight.savefig('stations_four_eight.pdf')
 stations_eight_twenty.savefig('stations_eight_twenty.pdf')
 stations_twenty_forty.savefig('stations_twenty_forty.pdf')
+stations_two_four_eight.savefig('stations_two_four_eight.pdf')
+stations_four_eight_twenty.savefig('stations_four_eight_twenty.pdf')
+stations_eight_twenty_forty.savefig('stations_eight_twenty_forty.pdf')
 
 plt.show()
 
