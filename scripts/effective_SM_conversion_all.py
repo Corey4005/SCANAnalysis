aa8fb8491cda8de1a79cd3348dfaef5e8e991065
Corey4005 <cdw0063@uah.edu>
2021-12-03 14:53:02 -0600

    finished first part of ESM function to allow broad calculations given dictionary of soil values

--- scripts/effective_SM_conversion_all.py
+++ scripts/effective_SM_conversion_all.py
@@ -9,7 +9,7 @@ import pandas as pd
 import numpy as np
 
 #read in the ESI data
-GOES_ESI_ALL = '../../Data/Processed_ESI/1_wk_ESI_all.csv'
+GOES_ESI_ALL = '../data/1_wk_ESI_all.csv'
 GOES_READ = pd.read_csv(GOES_ESI_ALL)
 
 #read in the SCAN data
@@ -20,17 +20,27 @@ SCAN_READ = pd.read_csv(SCAN_ALL)
 SMS = SCAN_READ[['Date', 'station','SMS-2.0in', 'SMS-4.0in', 'SMS-8.0in', 'SMS-20.0in','SMS-40.0in']].copy()
 SMS['Date'] = pd.to_datetime(SMS['Date'])
 
+#create a test variable to run the code on to see if it works 
+TEST = SMS[SMS['station'] == '2057:AL:SCAN']
+#TEST.dropna(inplace=True)
 
 # #effective SM
 # (convert_column - theta_r)/(Porosity - theta_r)
 
+#Calculate particle density 
+#Dp = 100 / (((1.7 * OC) / Dp1) + ((1.6 * FE)/Dp2) + ((100 - ((1/7 * OC) + (1.6 * FE)))/ Dp3))
 def SoilType(SMS):
     dict_list = []
     for i in SMS['station']:
         if i == '2057:AL:SCAN':
             soil_dict = {'two':'silty clay loam', 'four':'silty clay loam', 
                          'eight':'silty clay loam', 'twenty':'silty clay loam', 
-                         'forty':'silty clay loam'}
+                         'forty':'silty clay loam', 'OC2in': 1.9, 'OC4in': 1.1, 
+                         'OC8in': 1.1, 'OC20in': 0.2, 'OC40in': 0.2, 'FE2in': 2.3,
+                         'FE4in': 1.5, 'FE8in': 1.5, 'FE20in': 2.2, 'FE40in': 3.8, 
+                         'Db2in': 1.53, 'Db4in': 1.55, 'Db8in': 1.55, 'Db20in': 1.39,
+                         'Db40in': 1.49, 'Kpa2in': 13.5, 'Kpa4in': 10.4, 
+                         'Kpa8in': 10.4, 'Kpa20in': 15.2, 'Kpa40in': 20.7}
             dict_list.append(soil_dict)
         elif i == '2078:AL:SCAN':
             soil_dict = {'two':'silty clay loam', 'four':'silty clay', 
@@ -61,23 +71,98 @@ def SoilType(SMS):
                          'eight':'silt loam', 'twenty': 'silty clay', 
                          'forty': 'silty clay'}
             dict_list.append(soil_dict)
-        elif i == '2173:AL:SCAN': 
-            
+        elif i == '2173:AL:SCAN':
+            soil_dict = {}
+            dict_list.append(soil_dict)
+        
     SMS['Soil Class Dictionary'] = dict_list
     return SMS
 
-def GetThetar_Two(SMS):
-    two_list = []
-    two_thetar = []
+def CalculateESM(SMS):
+    #set some knowns for conversion
+    Dp1 = 1.4
+    Dp2 = 4.2 
+    Dp3 = 2.65
+    lis_dict = []
+    station = []
+    #station is an object and will not be preserved in numerical calculations
+    #therefore we will store it in a list to append it back to df later
+    for i in SMS['station']:
+        station.append(i)
+        
+    for i in SMS['Soil Class Dictionary']:
+        #get all the variables we need for soil column calculations
+        OC2in = i.get('OC2in')
+        OC4in = i.get('OC4in')
+        OC8in = i.get('OC8in')
+        OC20in = i.get('OC20in')
+        OC40in = i.get('OC40in')
+        FE2in = i.get('FE2in')
+        FE4in = i.get('FE4in')
+        FE8in = i.get('FE8in')
+        FE20in = i.get('FE20in')
+        FE40in = i.get('FE40in')
+        Kpa2in = i.get('Kpa2in')
+        Kpa4in = i.get('Kpa4in')
+        Kpa8in = i.get('Kpa8in')
+        Kpa20in = i.get('Kpa20in')
+        Kpa40in = i.get('Kpa40in')
+        Db2in = i.get('Db2in')
+        Db4in = i.get('Db4in')
+        Db8in = i.get('Db8in')
+        Db20in = i.get('Db20in')
+        Db40in = i.get('Db40in')
+        
+        #we need to preserve the soil class dictionary for later as well so 
+        #that it is not lost
+        lis_dict.append(i)
+        
+        
+        #calculate particle density for each level in soil column 
+        Dp2in = 100 / (((1.7 * OC2in) / Dp1) + ((1.6 * FE2in)/Dp2) + 
+                     ((100 - ((1/7 * OC2in) + (1.6 * FE2in)))/ Dp3))
+        
+        Dp4in = 100 / (((1.7 * OC4in) / Dp1) + ((1.6 * FE4in)/Dp2) + 
+                     ((100 - ((1/7 * OC4in) + (1.6 * FE4in)))/ Dp3))
+        
+        Dp8in = 100 / (((1.7 * OC8in) / Dp1) + ((1.6 * FE8in)/Dp2) + 
+                     ((100 - ((1/7 * OC8in) + (1.6 * FE8in)))/ Dp3))
+        
+        Dp20in = 100 / (((1.7 * OC20in) / Dp1) + ((1.6 * FE20in)/Dp2) + 
+                     ((100 - ((1/7 * OC20in) + (1.6 * FE20in)))/ Dp3))
+        
+        Dp40in = 100 / (((1.7 * OC40in) / Dp1) + ((1.6 * FE40in)/Dp2) + 
+                     ((100 - ((1/7 * OC40in) + (1.6 * FE40in)))/ Dp3))
+        
+        #calculate porosity for each level
+        #Calculate Porosity
+        P2in = 100 - (100 * Db2in/Dp2in)
+        P4in = 100 - (100 * Db4in/Dp4in)
+        P8in = 100 - (100 * Db8in/Dp8in)
+        P20in = 100 - (100 * Db20in/Dp20in)
+        P40in = 100 - (100 * Db40in/Dp40in)
+        #calculate thetar for each level in soil column
+        theta_r_two = 0.35 * Kpa2in
+        theta_r_four = 0.35 * Kpa4in
+        theta_r_eight = 0.35 * Kpa8in 
+        theta_r_twenty = 0.35 * Kpa20in 
+        theta_r_forty = 0.35 * Kpa40in 
+        
+        #calculate esm 
+        SMS['2in_esm'] = (SMS['SMS-2.0in'] - theta_r_two)/(P2in - theta_r_two)
+        SMS['4in_esm'] = (SMS['SMS-4.0in'] - theta_r_four)/(P4in - theta_r_four)
+        SMS['8in_esm'] = (SMS['SMS-8.0in'] - theta_r_eight)/(P8in - theta_r_eight)
+        SMS['20in_esm'] = (SMS['SMS-20.0in'] - theta_r_twenty)/(P20in - theta_r_twenty)
+        SMS['40in_esm'] = (SMS['SMS-40.0in'] - theta_r_forty)/(P40in - theta_r_forty)
     
-    for i in SMS['Soil Dictionary']:
-        twoin = i.get('two')
-        two_list.append(twoin)
-    for i in two_list: 
-        if i == 'silty clay loam':
-            theta_r = 0.070
-            two_thetar.append(theta_r)
-    return SMS['2in Theta_r']
+    SMS['station'] = station
+    SMS['Soil Dictionary'] = lis_dict
+    NEW_DF = SMS[['Date', 'station', 'Soil Dictionary', '2in_esm', '4in_esm', '8in_esm', '20in_esm', '40in_esm']]
+    return NEW_DF
+        
+        
+    
+    
     
     
 # 2173:AL:SCAN
